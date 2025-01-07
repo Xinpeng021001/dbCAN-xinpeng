bf15f42ce944f88fe56473dbedb0396dbea0e377
Xinpeng Zhang <84593788+Xinpeng021001@users.noreply.github.com>
2025-01-07 16:06:15 -0600

    Update process_dbcan_sub.py

--- finished_script/process_dbcan_sub.py
+++ finished_script/process_dbcan_sub.py
@@ -16,8 +16,8 @@ class DBCANProcessor:
         """
         try:
             df = pd.read_csv(self.mapping_file, sep='\t', header=None, skiprows=1, usecols=[2, 4, 0])
-            df[4] = df[4].str.strip()
-            df['key'] = df.apply(lambda x: (x[2], x[4]) if x[4] else (x[2], '-'), axis=1)
+            df[4] = df[4].str.strip().fillna('-')
+            df['key'] = df.apply(lambda x: (x[2], x[4]) , axis=1)
             return pd.Series(df[0].values, index=pd.MultiIndex.from_tuples(df['key'])).to_dict()
         except FileNotFoundError:
             print(f"can't find file: {self.mapping_file}")
